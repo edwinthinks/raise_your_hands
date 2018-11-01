3ea7de063a13ec2f0028cb02a75914c2e86bc459
Edwin Mak <cintosyntax@gmail.com>
2018-10-31 20:12:16 -0400

    Fix tests

--- test/raise_your_hands_web/controllers/page_controller_test.exs
+++ test/raise_your_hands_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule RaiseYourHandsWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Raise Your Hand!"
   end
 end
